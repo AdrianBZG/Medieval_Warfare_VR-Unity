c3445c5bda90645dfdc935ef8ef987fe4bd1ba14
Rudolf <rudolfcicko2016@gmail.com>
2016-10-16 14:52:49 +0100

    Added monster and event emitter for running :japanese_goblin:
    Now when the player reach a determine area the monster starts to run
    through you

---
+++ Assets/RootMovionScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Animator))]
+public class RootMovionScript : MonoBehaviour {
+
+    bool canWalk;
+    public float runSpeed;
+
+    void OnEnable()
+    {
+        EventsManager.onAreaReached += CanWalk;
+    }
+
+
+    void OnDisable()
+    {
+        EventsManager.onAreaReached -= CanWalk;
+    }
+
+    void Start ()
+    {
+        canWalk = false;
+    }
+
+    void OnAnimatorMove()
+    {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator && canWalk)
+        {
+            print("CAN run!!");
+            Vector3 newPosition = transform.position;
+            newPosition.x += animator.GetFloat("Runspeed") * runSpeed;
+            transform.position = newPosition;
+        }
+    }
+
+    public void CanWalk ()
+    {
+        canWalk = true;
+    }
+}
