755afe2d11362e219a111d2e86e9cc5c40091d32
AdrianBZG <arodriba@ull.es>
2016-11-21 22:56:49 +0000

    First AIAgent script with damage functions, and die die animation playing

---
+++ Assets/AIAgent.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIAgent : MonoBehaviour {
+
+	private int lifePoints = 100;
+	private bool dead = false;
+	public GameObject agentEntity = null;
+	public GameObject agentEngine = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int getLifePoints() {
+		return lifePoints;
+	}
+
+	public void setLifePoints(int lifePoints) {
+		this.lifePoints = lifePoints;
+	}
+
+	private void checkDead() {
+		if (this.lifePoints < 0 && !dead) {
+			dead = true;
+			agentEntity.SetActive (false);
+			agentEngine.SetActive (false);
+			GetComponent<Animation>().Play ("die1", PlayMode.StopAll);
+		}
+	}
+
+	public void getDamage() {
+		this.lifePoints -= 20;
+		checkDead ();
+	}
+
+	public void getDamage(int damage) {
+		this.lifePoints -= damage;
+		checkDead ();
+	}
+		
+	public bool isDead() {
+		return dead;
+	}
+}
