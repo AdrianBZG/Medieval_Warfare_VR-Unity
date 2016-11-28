06b3ae4bc6022e7c949f13341bd5040ca3a3bfcf
AdrianBZG <arodriba@ull.es>
2016-11-28 23:24:58 +0000

    Adding MenuButtonManager script

---
+++ Assets/Scripts/Menu/MenuButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonManager : MonoBehaviour {
+
+	public void NewGame(string levelName) {
+		SceneManager.LoadScene (levelName);
+	}
+
+	public void ShowDevelopers() {
+		//
+	}
+
+	public void ShowManual() {
+		//
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
