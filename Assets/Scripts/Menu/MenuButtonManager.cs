de5571f488e9dd64aa261197dfb43cce57849b35
AdrianBZG <arodriba@ull.es>
2016-11-28 23:56:09 +0000

    Menu buttons are working now

--- Assets/Scripts/Menu/MenuButtonManager.cs
+++ Assets/Scripts/Menu/MenuButtonManager.cs
@@ -4,19 +4,37 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtonManager : MonoBehaviour {
 
+	public GameObject buttons;
+	public GameObject developers;
+	public GameObject manual;
+
+
+	void Start() {
+		developers.SetActive (false);
+		manual.SetActive (false);
+	}
+
 	public void NewGame(string levelName) {
 		SceneManager.LoadScene (levelName);
 	}
 
 	public void ShowDevelopers() {
-		//
+		developers.SetActive (true);
+		buttons.SetActive (false);
 	}
 
 	public void ShowManual() {
-		//
+		manual.SetActive (true);
+		buttons.SetActive (false);
 	}
 
 	public void QuitGame() {
 		Application.Quit ();
+	}
+
+	public void BackButton() {
+		developers.SetActive (false);
+		manual.SetActive (false);
+		buttons.SetActive (true);
 	}
 }
