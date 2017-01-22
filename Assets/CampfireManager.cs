ec3f4d93bbce8bf890303b686fd8d06596ae3ed2
Rudolf <rudolfcicko2016@gmail.com>
2017-01-22 23:04:53 +0000

    Fixing trolls invokation

--- Assets/CampfireManager.cs
+++ Assets/CampfireManager.cs
@@ -16,7 +16,7 @@ public class CampfireManager : MonoBehaviour, IGvrGazeResponder {
 	
 	}
 
-
+    // Switch between on and off.
 	public void Switch () {
 		if (IsOn()) 
 			TurnOff();
@@ -24,30 +24,36 @@ public class CampfireManager : MonoBehaviour, IGvrGazeResponder {
 			TurnOn();
 	}
 
+    // Turn the campfire On
 	public void TurnOn () {
-		SetTurn(true);
+		Turn(true);
 	}
 
+    // Turn the campfire Off
 	public void TurnOff () {
-		SetTurn(false);
+		Turn(false);
 	}
 
-	private void SetTurn (bool turningOn) {
+    // This method allows to turn on or off the campfire.
+	private void Turn (bool turningOn) {
 		isOn = turningOn;
 		fireParticles.gameObject.SetActive(turningOn);
 		sparkParticles.gameObject.SetActive(turningOn);
 		fireLight.gameObject.SetActive(turningOn);
 	}
 
+    // This method returns true if the campfire is on.
 	public bool IsOn () {
 		return isOn;
 	}
 
+    // When the player looks at the campfire it turns on.
     void IGvrGazeResponder.OnGazeEnter()
     {
         TurnOn();
     }
 
+    // When the player look away the campfire turns off.
     void IGvrGazeResponder.OnGazeExit()
     {
         TurnOff();
@@ -55,5 +61,6 @@ public class CampfireManager : MonoBehaviour, IGvrGazeResponder {
 
     void IGvrGazeResponder.OnGazeTrigger()
     {
+        // Nothing to do
     }
 }
--- Assets/TrollSpawnManager.cs
+++ Assets/TrollSpawnManager.cs
@@ -5,6 +5,11 @@ public class TrollSpawnManager : MonoBehaviour {
 
     public GameObject trollPrefab;
     public float spawnInterval;
+
+    public Transform[] spawnPositions;
+    private int inx = 0;
+
+
     private static bool keyCaptured = false;
 
 
@@ -15,10 +20,16 @@ public class TrollSpawnManager : MonoBehaviour {
         InvokeRepeating("SpawnTroll", 0.0f, spawnInterval);
     }
 
+    Vector3 GetNextPosition ()
+    {
+        if (inx == spawnPositions.Length) inx = 0;
+        return spawnPositions[inx++ % spawnPositions.Length].position;
+    }
+
     void SpawnTroll ()
     {
         if (!GameManager.getPlayerHaveKey())
-            Instantiate(trollPrefab,transform.position, Quaternion.identity);
+            Instantiate(trollPrefab, GetNextPosition(), Quaternion.identity);
     }
 
     public static void KeyCaptured ()
